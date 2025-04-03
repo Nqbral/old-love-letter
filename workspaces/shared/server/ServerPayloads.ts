8f13ed0df034bd4fa4582d905bbf1feeebc0e7f4
Simon Chevalier <sim.chevalier@gmail.com>
2025-04-03 23:30:07 +0200

    Add server payloads

---
+++ workspaces/shared/server/ServerPayloads.ts
@@ -0,0 +1,16 @@
+import { ServerEvents } from "./ServerEvents";
+
+export type ServerPayloads = {
+  [ServerEvents.LobbyState]: {
+    lobbyId: string;
+    hasStarted: boolean;
+    hasFinished: boolean;
+    playersCount: number;
+    isSuspended: boolean;
+    scores: Record<string, number>;
+  };
+
+  [ServerEvents.GameMessage]: {
+    message: string;
+  };
+};
