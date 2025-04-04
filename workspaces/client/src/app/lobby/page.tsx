f81bff5d0e0e58b729829c3068dae565941b6491
Simon Chevalier <sim.chevalier@gmail.com>
2025-04-04 15:32:07 +0200

    CLIENT - Add Lobby page

---
+++ workspaces/client/src/app/lobby/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="container flex h-full min-h-screen w-full flex-col items-center justify-center gap-4">
+      <h1 className="">Lobby</h1>
+      <div className="flex flex-row gap-12"></div>
+    </div>
+  );
+}
