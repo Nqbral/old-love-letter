780bad03ec6095c0748b1203cdc2624f1f41dfe7
Simon Chevalier <sim.chevalier@gmail.com>
2025-04-04 16:06:03 +0200

    CLIENT - Add fontawesome icons dependencies + Add text on homepage

--- workspaces/client/src/app/page.tsx
+++ workspaces/client/src/app/page.tsx
@@ -1,3 +1,7 @@
+import { faComputer } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import desktopcomputer from '../../public/desktop-computer.svg';
 import LinkButton from './components/buttons/LinkButton';
 
 export default function Home() {
@@ -5,12 +9,12 @@ export default function Home() {
     <div className="container flex h-full min-h-screen w-full flex-col items-center justify-center gap-4">
       <h1 className="">Love Letter</h1>
       <div className="flex flex-row gap-12">
-        <LinkButton
-          buttonText={'Créer un lobby'}
-          linkTo={'lobby'}
-          primary={true}
-        />
+        <LinkButton buttonText={'Jouer'} linkTo={'lobby'} primary={true} />
         <LinkButton buttonText={'Règles'} linkTo={'rules'} primary={false} />
+      </div>
+      <div className="flex flex-row items-center gap-2">
+        <FontAwesomeIcon icon={faComputer} height={30} color="#8d9eaa" />
+        <p>Pour une expérience optimale, jouer sur un ordinateur</p>
       </div>
     </div>
   );
